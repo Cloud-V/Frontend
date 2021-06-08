010584c94e3ac5c9eb810fb5f5555559210d6891
Mohamed Gaber <me@donn.website>
2021-06-08 16:32:08 +0200

    Remove non-GitHub login options

--- src/views/LandingPage/index.js
+++ src/views/LandingPage/index.js
@@ -51,25 +51,30 @@ class LandingPage extends Component {
 						</Col>
 					</div>
 					<div className="landing-section cloudv-button cloudv-create-try d-flex justify-content-center w-100">
-						<Col xl="3" lg="4" md="5" sm="12" className="col-xxl-1-5 col-xs-12">
+						{/*<Col xl="3" lg="4" md="5" sm="12" className="col-xxl-1-5 col-xs-12">
 							<Button tag={Link} to={'/signup'} className="action-button w-100 mr-4 d-flex justify-content-center align-items-center">Create Account</Button>
-						</Col>
+		<				/Col>*/}
 						<Col xl="3" lg="4" md="5" sm="12" className="col-xxl-1-5 col-xs-12">
 							<Button tag={Link} to={'/try'} className="action-button w-100 action-button-outline d-flex justify-content-center align-items-center">Try Now</Button>
 						</Col>
 					</div>
+					{/*
 					<div className="landing-section cloudv-button">
 						Have an account? <Link className="plain-link" to={'/login'}><b>Log in</b></Link>
 					</div>
+					*/}
 					<div className="landing-section landing-section-bottom cloudv-auth-login d-flex flex-column align-items-center mt-5">
-						<h5 className="mb-4">Or log in using</h5>
+						<h5 className="mb-4">Log in</h5>
 						<div className="d-flex landing-section-auth-buttons">
-							<Col md="6" sm="12" className="mr-1">
 								<GHSignin />
+							{/*
+							
+							<Col md="6" sm="12" className="mr-1">
 							</Col>
 							<Col md="6" sm="12" className="">
 								<GSignin />
 							</Col>
+							*/}
 						</div>
 					</div>
 					<div className="landing-section landing-section-bottom cloudv-landing-footer d-flex flex-column align-items-center">
