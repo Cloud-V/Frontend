37243c80cd81ad0f69ddec7ac104dbfe3b5743de
Mohamed Gaber <me@donn.website>
2021-08-16 07:46:41 +0300

    Restore Login Features

--- src/views/LandingPage/index.js
+++ src/views/LandingPage/index.js
@@ -14,7 +14,7 @@ import {
 	Row
 } from 'reactstrap';
 import Parallax from 'parallax-js';
-import { /* GSignin, */ GHSignin } from 'partials/SocialButtons';
+import { GSignin, GHSignin } from 'partials/SocialButtons';
 
 import Clouds from './Clouds';
 
@@ -47,34 +47,28 @@ class LandingPage extends Component {
 					</div>
 					<div className="landing-section cloudv-slogan-subtext center-all-text">
 						<Col lg="12" md="10">
-							Cloud V is a research project that allows you to design and test your chips online <b>for free</b>.
+							Cloud V is a research project that allows you to design and test your chips online, free of charge.
 						</Col>
 					</div>
 					<div className="landing-section cloudv-button cloudv-create-try d-flex justify-content-center w-100">
-						{/*<Col xl="3" lg="4" md="5" sm="12" className="col-xxl-1-5 col-xs-12">
+						<Col xl="3" lg="4" md="5" sm="12" className="col-xxl-1-5 col-xs-12">
 							<Button tag={Link} to={'/signup'} className="action-button w-100 mr-4 d-flex justify-content-center align-items-center">Create Account</Button>
-		<				/Col>*/}
+						</Col>
 						<Col xl="3" lg="4" md="5" sm="12" className="col-xxl-1-5 col-xs-12">
 							<Button tag={Link} to={'/try'} className="action-button w-100 action-button-outline d-flex justify-content-center align-items-center">Try Now</Button>
 						</Col>
 					</div>
-					{/*
+					
 					<div className="landing-section cloudv-button">
 						Have an account? <Link className="plain-link" to={'/login'}><b>Log in</b></Link>
 					</div>
-					*/}
+					
 					<div className="landing-section landing-section-bottom cloudv-auth-login d-flex flex-column align-items-center mt-5">
-						<h5 className="mb-4">Log in</h5>
+						<h5 className="mb-4">Or log in with…</h5>
 						<div className="d-flex landing-section-auth-buttons">
-								<GHSignin />
-							{/*
-							
-							<Col md="6" sm="12" className="mr-1">
-							</Col>
-							<Col md="6" sm="12" className="">
-								<GSignin />
-							</Col>
-							*/}
+							<GHSignin />
+							<span style={{width: "30px"}}></span>
+							<GSignin />
 						</div>
 					</div>
 					<div className="landing-section landing-section-bottom cloudv-landing-footer d-flex flex-column align-items-center">
